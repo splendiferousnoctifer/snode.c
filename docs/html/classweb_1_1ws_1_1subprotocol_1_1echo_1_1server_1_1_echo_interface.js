65bd8bd8ac14746f0232df9ddc9db00b2936f88d
Volker Christian <volker.christian@fh-hagenberg.at>
2021-06-13 21:56:28 +0200

    Update docs

--- docs/html/classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.js
+++ docs/html/classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.js
@@ -1,6 +1,6 @@
 var classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface =
 [
-    [ "create", "classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.html#ad3a30383ea53a4319cd02462825b4d5b", null ],
+    [ "create", "classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.html#afff32c0b87803ca947960353d7ec2444", null ],
     [ "destroy", "classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.html#af7966de7d1764ba1cefe05131ef3cac1", null ],
     [ "name", "classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.html#af3213225454a1e51788dfc19519934aa", null ],
     [ "role", "classweb_1_1ws_1_1subprotocol_1_1echo_1_1server_1_1_echo_interface.html#a72498483029ce22b77ed197d3b4f3f35", null ]
